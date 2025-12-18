a4d19e1162bcb227c85b3a32cbac6a431eb24435
Shristy Joshi Thakur <shristyjoshitk2001@gmail.com>
2025-12-19 01:06:36 +0530

    Final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,12 @@
 // export default defineConfig({ plugins: [react()] });
 
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Use plain esbuild JSX transform instead of @vitejs/plugin-react
 export default defineConfig({
+    plugins: [react()],
+  base: "/", // 🔑 CRITICAL for Railway static hosting
   esbuild: {
     jsxInject: `import React from "react"`,
     jsxFactory: "React.createElement",
